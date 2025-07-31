9713d0ce3837efe3bbc8d0afc27ea25d24501528
Arjun <einsteinarjun101@gmail.com>
2025-07-31 17:09:06 +0530

    Updated search & get

--- single_linked_list.py
+++ single_linked_list.py
@@ -1,13 +1,13 @@
 class Node:
-    def __init__ (self,value):  # Constructor for Node creation
+    def __init__ (self,value): 
         self.value = value
         self.next = None
 
 class LinkedList:
-    def __init__ (self): # Constructor for creating linked list with 1st node
+    def __init__ (self):
         self.head = None
         self.tail = None
-        self.length = 0 # Don't forget to give length = 1 when creating node in linkedlist
+        self.length = 0
         
     def __str__ (self):
         temp_node = self.head
@@ -19,15 +19,15 @@ class LinkedList:
             temp_node = temp_node.next
         return result
     
-    def append(self,value): # O(1)
-        new_node = Node(value)  # create node
-        if self.head is None:    # If the length is already 0 just create new_node 
-            self.head =  new_node   # and make head & tail as new_node  
+    def append(self,value): 
+        new_node = Node(value)
+        if self.head is None: 
+            self.head =  new_node  
             self.tail = new_node
-        else:                   # If it already has elements,
-            self.tail.next = new_node   # then make new_node was tail.next
+        else:                  
+            self.tail.next = new_node
             self.tail = new_node
-        self.length += 1        # Increase length
+        self.length += 1  
     
     def prepend(self,value):
         new_node = Node(value)
@@ -35,9 +35,9 @@ class LinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            new_node.next = self.head   # Making the pointer of the new_node to point towards the self.head (element)
-            self.head = new_node    # Now assigning self.head as the new_node
-        self.length += 1            # Increasing the length once prepended    
+            new_node.next = self.head  
+            self.head = new_node 
+        self.length += 1           
         
     def insert(self,value,index):
         new_node = Node(value)
@@ -60,27 +60,67 @@ class LinkedList:
             temp_node.next = new_node
         self.length += 1
         return True
-        
+    
+    def traverse(self):
+        current = self.head
+        while current is not None:
+            print(current.value)
+            current = current.next
+            
+    def search(self,target):
+        current = self.head
+        while current is not None:
+            if current.value == target:
+                return True
+            current = current.next
+        return False
+    
+    def search_index(self,target):
+        current = self.head
+        index = 0
+        while current is not None:
+            if current.value == target:
+                return index
+            current = current.next
+            index += 1
+        return -1
+    
+    def get (self,index):
+        if index < -1 or index >= self.length:
+            return None
+        if index == -1:
+            return self.tail
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current
+    
 new_linked_list = LinkedList()
+
+print("Inital linked_list: ",new_linked_list.head)
 
 new_linked_list.append(10)
 new_linked_list.append(20)
 new_linked_list.append(30)
 new_linked_list.append(40)
-
+print("Appending items, 10, 20, 30, 40: ")
 print(new_linked_list)
 
+print("Inserting item 0 at index 0: ")
 new_linked_list.insert(0,0)
-
 print(new_linked_list)
 
-new_linked_list.insert(25,3)
-
+print("Inserting item 50 at index 5: ")
+new_linked_list.insert(50,5)
 print(new_linked_list)
 
-new_linked_list.insert(50,6)
+print("Traversing inside the linked-list:")
+new_linked_list.traverse()
 
-print(new_linked_list)
+print("Is item present in the linked-list? ",new_linked_list.search(30))
+print("Index of the item: ",new_linked_list.search_index(30))
+
+print("Get the item using index (Here index is 3): ",new_linked_list.get(3).value)
 
 print("This is head: ",new_linked_list.head.value)
 print("This is tail: ",new_linked_list.tail.value)
